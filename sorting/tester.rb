2425820406322c261716faceaf42a77a322e343d
Oscar Lee <oscarwinglee@gmail.com>
2024-08-21 09:29:08 -0400

    init tester and sortable

---
+++ sorting/tester.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require_relative 'sortable'
+
+class Tester
+  extend Sortable
+
+  attr_accessor :config, :data
+
+  def initialize(data = nil, config = {})
+    @data = data.nil? ? (1..10).to_a.shuffle : data
+  end
+
+  def config=(args)
+    self.config.merge!(args)
+  end
+
+  def sample_size=(n)
+    self.data ||= []
+    self.data[n] = nil
+    n
+  end
+
+  def pp(arr)
+    puts "STEP=[#{arr.join(", ")}]"
+  end
+
+  # calls the sort method with new data; retains original shuffled data
+  (self.methods - Object.methods).each do |method_name|
+    define_method(method_name) {
+      self.class.send(method_name, self.data.class.new(self.data))
+    }
+  end
+end
